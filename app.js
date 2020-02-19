4637f01a78962d8d7d0362bf2464726147cbfbac
Kyle <kyle.fetterhoff@gmail.com>
2020-02-19 10:33:28 -0500

    Add initial index route

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.json({ extended: false }));
+
+// Set static folder
+app.use(express.static(`${__dirname}/public`));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
