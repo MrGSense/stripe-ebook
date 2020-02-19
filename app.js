13ad45b9da20f6aaf965915f9f720d48b0300d41
Kyle <kyle.fetterhoff@gmail.com>
2020-02-19 11:27:35 -0500

    Fix charge

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post("/charge", (req, res) => {
     .then(customer =>
       stripe.charges.create({
         amount,
-        description: "Internet Etiquette Ebook",
+        description: "Internet Etiquitte Ebook",
         currency: "usd",
         customer: customer.id
       })
